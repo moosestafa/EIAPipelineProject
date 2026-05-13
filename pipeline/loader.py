b2ac2c50d4fc3a50cffc18a1205c5d6b3cb32e5b
MDaredia <60297592+moosestafa@users.noreply.github.com>
2026-05-13 01:20:29 +0000

    directory creation for ignored folder

--- pipeline/loader.py
+++ pipeline/loader.py
@@ -8,6 +8,7 @@ load_dotenv()
 
 #converts the dataframe to a parquet file and uploads it to S3 
 def save_parquet(df,state,year,month):
+    os.makedirs('data/raw', exist_ok=True)
     df.to_parquet(f"data/raw/{state}-{year}-{month}.parquet")
     s3_uploader = boto3.client('s3',aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
                                aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),region_name = os.getenv("AWS_REGION") )
