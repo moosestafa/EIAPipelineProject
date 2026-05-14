1951b0250310122e4d12d42e186b6af6f250dcea
MDaredia <60297592+moosestafa@users.noreply.github.com>
2026-05-14 14:51:35 -0500

    Refine data cleaning to filter MCRFP series
    Filter data to only include MCRFP series and clean unnecessary columns.

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -9,10 +9,11 @@ def create_dataframe(data):
     return df
 #drops unneccessary columns, and makes sure that the correct units are being read in, then drops units table
 def clean_data(df):
+    df = df[df["series"].str.startswith("MCRFP")]
     df=df.drop(columns=['duoarea','product', 'product-name', 'process','process-name', 'series', 'series-description'])
     df = df[df["units"] == "MBBL"]    
     df= df.drop(columns=["units"])
-    df = df[df["series"].str.startswith("MCRFP")]
+    
     return df
 #sorts data by month, and converts to proper data types
 def transform_data(df):
