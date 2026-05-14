d44f3ce1b73a8da91c9da0c16150c76abbe238fb
MDaredia <60297592+moosestafa@users.noreply.github.com>
2026-05-14 17:48:27 +0000

    error handling

--- pipeline/eia_client.py
+++ pipeline/eia_client.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+from requests.exceptions import HTTPError
 
 load_dotenv()
 #fetches api key
@@ -11,7 +12,10 @@ eia_key = os.getenv("EIA_API_KEY")
 def fetch_data(state, start_month, end_month):
     url = f"https://api.eia.gov/v2/petroleum/crd/crpdn/data/?frequency=monthly&data[0]=value&facets[duoarea][]=S{state}&start={start_month}&end={end_month}&sort[0][column]=duoarea&sort[0][direction]=desc&offset=0&length=5000&api_key={eia_key}"
     response = requests.get(url)
-    response.raise_for_status()
+    try:
+        response.raise_for_status()
+    except HTTPError:
+        raise HTTPError(f"Request failed with status code {response.status_code}")
     return response.json()
 """"
 data = fetch_data("TX","2026-01","2026-01")
